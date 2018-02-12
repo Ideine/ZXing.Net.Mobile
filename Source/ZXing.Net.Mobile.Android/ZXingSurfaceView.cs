d9ae3ff7283dc04ebffa32135ca6eb031d26356d
Valentin Jubert <valentin.jubert@ideine.fr>
2018-02-12 15:25:35 +0100

    Clean and correct crash

--- Source/ZXing.Net.Mobile.Android/ZXingSurfaceView.cs
+++ Source/ZXing.Net.Mobile.Android/ZXingSurfaceView.cs
@@ -3,6 +3,7 @@ using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Graphics;
+using ZXing.Net.Mobile.Android;
 using ZXing.Mobile.CameraAccess;
 using System.Threading.Tasks;
 
@@ -12,33 +13,37 @@ namespace ZXing.Mobile
 	{
 		private readonly TaskCompletionSource<bool> _initialized = new TaskCompletionSource<bool>();
 
+		private CameraAnalyzer _cameraAnalyzer;
+
+		private bool _holderCallbackAdded = false;
+
+		private Action<Result> _scanResultCallback;
+
+		public MobileBarcodeScanningOptions ScanningOptions { get; set; }
+
 		public ZXingSurfaceView(Context context, MobileBarcodeScanningOptions options) : base(context)
 		{
 			ScanningOptions = options ?? new MobileBarcodeScanningOptions();
 			Init();
 		}
 
-		protected ZXingSurfaceView(IntPtr javaReference, JniHandleOwnership transfer)
-			: base(javaReference, transfer)
+		protected ZXingSurfaceView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
 		{
 			Init();
 		}
 
-		bool addedHolderCallback = false;
-
 		private void Init()
 		{
-			if (!addedHolderCallback)
+			if (!_holderCallbackAdded)
 			{
-				this.SurfaceTextureListener = this;
-				addedHolderCallback = true;
+				SurfaceTextureListener = this;
+				_holderCallbackAdded = true;
 			}
 		}
 
 		public async void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
 		{
-			await ZXing.Net.Mobile.Android.PermissionsHandler.PermissionRequestTask;
-
+			await PermissionsHandler.PermissionRequestTask;
 
 			if (_cameraAnalyzer == null)
 			{
@@ -55,10 +60,10 @@ namespace ZXing.Mobile
 		{
 			try
 			{
-				if (addedHolderCallback)
+				if (_holderCallbackAdded)
 				{
-					this.SurfaceTextureListener = null;
-					addedHolderCallback = false;
+					SurfaceTextureListener = null;
+					_holderCallbackAdded = false;
 				}
 			}
 			catch
@@ -72,15 +77,12 @@ namespace ZXing.Mobile
 
 		public async void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
 		{
-			await ZXing.Net.Mobile.Android.PermissionsHandler.PermissionRequestTask;
+			await PermissionsHandler.PermissionRequestTask;
 
 			_cameraAnalyzer?.RefreshCamera();
 		}
 
-		public void OnSurfaceTextureUpdated(SurfaceTexture surface)
-		{
-
-		}
+		public void OnSurfaceTextureUpdated(SurfaceTexture surface) { }
 
 		public override bool OnTouchEvent(MotionEvent e)
 		{
@@ -100,66 +102,31 @@ namespace ZXing.Mobile
 			return r;
 		}
 
-		public void AutoFocus()
-		{
-			_cameraAnalyzer.AutoFocus();
-		}
-
-		public void AutoFocus(int x, int y)
-		{
-			_cameraAnalyzer.AutoFocus(x, y);
-		}
-
 		public async void StartScanning(Action<Result> scanResultCallback, MobileBarcodeScanningOptions options = null)
 		{
+			_scanResultCallback = scanResultCallback;
+
 			ScanningOptions = options ?? MobileBarcodeScanningOptions.Default;
 
 			await _initialized.Task;
 
-			_cameraAnalyzer.BarcodeFound += (sender, result) =>
-			{
-				scanResultCallback?.Invoke(result);
-			};
+			_cameraAnalyzer.BarcodeFound += OnBarcodeFound;
+
 			_cameraAnalyzer.ResumeAnalysis();
 		}
 
-		public void StopScanning()
-		{
-			_cameraAnalyzer.ShutdownCamera();
-		}
-
-		public void PauseAnalysis()
-		{
-			_cameraAnalyzer.PauseAnalysis();
-		}
-
-		public void ResumeAnalysis()
-		{
-			_cameraAnalyzer.ResumeAnalysis();
-		}
 
 		public void Torch(bool on)
 		{
 			if (on)
+			{
 				_cameraAnalyzer.Torch.TurnOn();
+			}
 			else
+			{
 				_cameraAnalyzer.Torch.TurnOff();
+			}
 		}
-
-		public void ToggleTorch()
-		{
-			_cameraAnalyzer.Torch.Toggle();
-		}
-
-		public MobileBarcodeScanningOptions ScanningOptions { get; set; }
-
-		public bool IsTorchOn => _cameraAnalyzer.Torch.IsEnabled;
-
-		public bool IsAnalyzing => _cameraAnalyzer.IsAnalyzing;
-
-		private CameraAnalyzer _cameraAnalyzer;
-
-		public bool HasTorch => _cameraAnalyzer.Torch.IsSupported;
 
 		protected override void OnAttachedToWindow()
 		{
@@ -172,8 +139,44 @@ namespace ZXing.Mobile
 		protected override void OnWindowVisibilityChanged(ViewStates visibility)
 		{
 			base.OnWindowVisibilityChanged(visibility);
+
 			if (visibility == ViewStates.Visible)
+			{
 				Init();
+			}
 		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (_cameraAnalyzer != null)
+				{
+					_cameraAnalyzer.BarcodeFound -= OnBarcodeFound;
+					_cameraAnalyzer = null;
+				}
+			}
+			base.Dispose(disposing);
+		}
+
+		private void OnBarcodeFound(object sender, Result result) => _scanResultCallback?.Invoke(result);
+
+		public bool HasTorch => _cameraAnalyzer?.Torch?.IsSupported ?? false;
+
+		public bool IsTorchOn => _cameraAnalyzer?.Torch?.IsEnabled ?? false;
+
+		public bool IsAnalyzing => _cameraAnalyzer?.IsAnalyzing ?? false;
+
+		public void StopScanning() => _cameraAnalyzer?.ShutdownCamera();
+
+		public void PauseAnalysis() => _cameraAnalyzer?.PauseAnalysis();
+
+		public void ResumeAnalysis() => _cameraAnalyzer?.ResumeAnalysis();
+
+		public void AutoFocus() => _cameraAnalyzer?.AutoFocus();
+
+		public void AutoFocus(int x, int y) => _cameraAnalyzer?.AutoFocus(x, y);
+
+		public void ToggleTorch() => _cameraAnalyzer?.Torch?.Toggle();
 	}
 }
